8435f74934a5a2ba94680ae7572ee1669110bb49
luckycmc <2287232750@qq.com>
2024-10-28 01:28:28 -0700

    test rpc

--- php/testlara/routes/web.php
+++ php/testlara/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(\App\Http\Controllers\RpcController::class)->group(function () {
+    Route::get('/rpc/test','test');
+});
